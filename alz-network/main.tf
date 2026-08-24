360595466faa774fdb5df4b45f420f8e5df13453
balazspados <balazs.pados@gmail.com>
2026-08-24 18:10:48 +1200

    reactivate PDNSZ

--- alz-network/main.tf
+++ alz-network/main.tf
@@ -252,34 +252,34 @@ rules = {}
 }
 
 ### Create private DNS zone
-# Deployed in management subscription see design documentation page 15.
-# resource "azurerm_resource_group" "private_dns_zone" {
-#   provider = azurerm.management
-#   location = local.azure_region_location
-#   name     = local.private_dns_zone.rg_name
-#   tags     = local.common_tags
+### Deployed in management subscription see design documentation page 15.
+resource "azurerm_resource_group" "private_dns_zone" {
+provider = azurerm.management
+location = local.azure_region_location
+name     = local.private_dns_zone.rg_name
+tags     = local.common_tags
 
-#   lifecycle {
-#     prevent_destroy = false
-#   }
-# }
+lifecycle {
+prevent_destroy = false
+}
+}
 
-# module "private_dns_zones" {
-#   source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
-#   version = "0.23.2" # https://registry.terraform.io/modules/Azure/avm-ptn-network-private-link-private-dns-zones/azurerm/latest
-#   providers = {
-#     azurerm = azurerm.management
-#   }
-#   location         = local.azure_region_location
-#   parent_id        = azurerm_resource_group.private_dns_zone.id
-#   enable_telemetry = local.avm_telemery_enable # Disabled now, https://azure.github.io/Azure-Verified-Modules/help-support/telemetry/
-#   tags             = local.common_tags
+module "private_dns_zones" {
+source  = "Azure/avm-ptn-network-private-link-private-dns-zones/azurerm"
+version = "0.23.2" # https://registry.terraform.io/modules/Azure/avm-ptn-network-private-link-private-dns-zones/azurerm/latest
+providers = {
+azurerm = azurerm.management
+}
+location         = local.azure_region_location
+parent_id        = azurerm_resource_group.private_dns_zone.id
+enable_telemetry = local.avm_telemery_enable # Disabled now, https://azure.github.io/Azure-Verified-Modules/help-support/telemetry/
+tags             = local.common_tags
 
-#   virtual_network_link_default_virtual_networks = {
-#     platform_vnet = {
-#       virtual_network_resource_id                 = azurerm_virtual_network.platform_vnet.id
-#       virtual_network_link_name_template_override = local.private_dns_zone_vnet_link.name_template        # due to naming convention
-#     }
-#   }
-# }
+virtual_network_link_default_virtual_networks = {
+platform_vnet = {
+virtual_network_resource_id                 = module.platform_vnet.resource_id
+virtual_network_link_name_template_override = local.private_dns_zone_vnet_link.name_template # overwrites default naming convention
+}
+  }
+}
 

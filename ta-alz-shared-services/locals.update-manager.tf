e2fbb012bda05808a4238103e1db79d431426331
balazspados <balazs.pados@gmail.com>
2026-09-16 13:14:31 +1200

    jhsdgfjsdfdsjgf

---
+++ ta-alz-shared-services/locals.update-manager.tf
@@ -0,0 +1,41 @@
+locals {
+  update_manager_parameters = {
+    rg_name = "${local.alz_config.org_id}-rg-update_manager-${local.alz_config.environment}-${local.alz_config.azure_region_location_short}-001"
+    osupdate_001 = {
+      name                     = "${local.alz_config.org_id}-mc-osupdate-${local.alz_config.environment}-${local.alz_config.azure_region_location_short}-001"
+      recur_every              = "4Weeks"
+      start_date_time          = "2026-09-10 22:00"
+      duration                 = "03:55" # Max value
+      time_zone                = "New Zealand Standard Time"
+      scope                    = "InGuestPatch"
+      scope_InGuestPatchMode   = "User" # Can either 'Platform' or 'User'
+      reboot_setting           = "IfRequired"
+      windows_included_updates = ["Critical", "Security"]
+      linux_included_updates   = ["Critical", "Security"]
+    }
+    osupdate_002 = {
+      name                     = "${local.alz_config.org_id}-mc-osupdate-${local.alz_config.environment}-${local.alz_config.azure_region_location_short}-002"
+      recur_every              = "Month Third Thursday"
+      start_date_time          = "2026-09-17 22:00"
+      duration                 = "03:55" # Max value
+      time_zone                = "New Zealand Standard Time"
+      scope                    = "InGuestPatch"
+      scope_InGuestPatchMode   = "User" # Can either 'Platform' or 'User'
+      reboot_setting           = "IfRequired"
+      windows_included_updates = ["Critical", "Security"]
+      linux_included_updates   = ["Critical", "Security"]
+    }
+    defupdate_001 = {
+      name                     = "${local.alz_config.org_id}-mc-defupdate-${local.alz_config.environment}-${local.alz_config.azure_region_location_short}-001"
+      recur_every              = "Day"
+      start_date_time          = "2026-09-08 20:00"
+      duration                 = "02:00"
+      time_zone                = "New Zealand Standard Time"
+      scope                    = "InGuestPatch"
+      scope_InGuestPatchMode   = "User" # Can either 'Platform' or 'User'
+      reboot_setting           = "IfRequired"
+      windows_included_updates = ["Definition"]
+      linux_included_updates   = []
+    }
+  }
+}
---
+++ ta-alz-shared-services/main.law-security.tf
@@ -0,0 +1,64 @@
+### Create LAW security resources
+
+### Create Private Endpoint in endpoint subnet
+resource "azurerm_monitor_private_link_scope" "ampls_law_security" {
+  provider            = azurerm.security
+  name                = local.law_security_parameters.monitor_private_link_scope_name
+  resource_group_name = azurerm_resource_group.rg_law_security.name
+  tags                = local.common_tags
+}
+
+resource "azurerm_monitor_private_link_scoped_service" "law_security" {
+  provider            = azurerm.security
+  name                = local.law_security_parameters.monitor_private_link_scoped_service_name
+  resource_group_name = azurerm_resource_group.rg_law_security.name
+  scope_name          = azurerm_monitor_private_link_scope.ampls_law_security.name
+  linked_resource_id  = module.law_security.resource_id
+}
+
+resource "azurerm_private_endpoint" "pep_law_security" {
+  provider            = azurerm.connectivity
+  name                = local.law_security_parameters.private_endpoint_name
+  location            = local.alz_config.azure_region_location
+  resource_group_name = local.law_security_parameters.private_endpoints_subnet_resource_group_name
+  subnet_id           = local.law_security_parameters.private_endpoints_subnet_id
+  tags                = local.common_tags
+
+  private_service_connection {
+    name                           = "psc-${local.law_security_parameters.private_endpoint_name}"
+    private_connection_resource_id = azurerm_monitor_private_link_scope.ampls_law_security.id
+    subresource_names              = ["azuremonitor"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "dnslinktovnet"
+    private_dns_zone_ids = local.law_security_parameters.private_dns_zone_ids
+  }
+}
+
+
+### Create LAW security
+
+module "law_security" {
+  source  = "Azure/avm-res-operationalinsights-workspace/azurerm"
+  version = "0.5.1" # https://github.com/Azure/terraform-azurerm-avm-res-operationalinsights-workspace
+  providers = {
+    azurerm = azurerm.security
+  }
+  location            = local.alz_config.azure_region_location
+  name                = local.law_security_parameters.law_name
+  resource_group_name = azurerm_resource_group.rg_law_security.name
+
+  log_analytics_workspace_internet_ingestion_enabled = local.law_security_parameters.log_analytics_workspace_internet_ingestion_enabled
+  log_analytics_workspace_internet_query_enabled     = local.law_security_parameters.log_analytics_workspace_internet_query_enabled
+  log_analytics_workspace_retention_in_days          = local.law_security_parameters.log_analytics_workspace_retention_in_days
+  # log_analytics_workspace_sku = local.law_security_parameters.log_analytics_workspace_sku
+  enable_telemetry = local.alz_config.telemetry_enabled # Disabled now, https://azure.github.io/Azure-Verified-Modules/help-support/telemetry/
+  tags             = local.common_tags
+
+
+  log_analytics_workspace_identity = {
+    type = "SystemAssigned"
+  }
+}
